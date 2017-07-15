fc04d16e45566a45b45356d713c6d7118efe9c0d
kyusulamita <sulamitamorales@yahoo.com>
2017-07-15 11:48:41 -0400

    initial

--- 1-functional-linked-list/list.js
+++ 1-functional-linked-list/list.js
@@ -3,19 +3,19 @@
 // This will come in handy when you implement ListNode.prototype.id
 const getSha1 = require('../getSha1');
 
-function ListNode (value, next) {
-  // your code here
-}
+// function ListNode (value, next) {
+//   // your code here
+// }
 
 /*
  * want to use ES6? Try writing this as a class instead:
  */
 
-// class ListNode {
-//   constructor (value, next) {
-//
-//   }
-// }
+class ListNode {
+  constructor (value, next) {
+
+  }
+}
 
 
 
